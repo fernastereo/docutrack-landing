79cb2445ca8774b0730df0f3333c9ad88f771237
Fernando Cueto <fernandoecueto@gmail.com>
2025-02-03 16:32:58 +0100

    add custom color palette to Tailwind configuration for enhanced theming options

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: {
+        100: '#75e69e',
+        200: '#4ade80',
+        300: '#26cf64',
+      },
+      base: {
+        100: '#ffffff',
+        200: '#c9c9c9',
+        300: '#2a2a2a',
+        400: '#111111',
+      },
+    },
     extend: {},
   },
   plugins: [],
 };
+
+
