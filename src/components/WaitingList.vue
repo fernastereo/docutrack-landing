ff013ddabaf8dc943fcfcf3729b7c313f970f00e
Fernando Cueto <fernandoecueto@gmail.com>
2025-02-05 11:58:23 +0100

    feat: integrate Firebase for waitlist functionality and enhance email validation in WaitingList component

--- src/components/WaitingList.vue
+++ src/components/WaitingList.vue
@@ -4,22 +4,23 @@
       <h2 class="text-md font-semibold mb-4 text-center" :class="isDarkMode ? 'text-base-100' : 'text-base-400'">
         Join Our Waitlist
       </h2>
-      <form @submit.prevent="handleSubmit" class="relative">
+      <div class="relative">
         <input
           type="email"
           v-model="email"
           placeholder="Enter your email"
           :class="[
-            'w-full px-4 py-4 rounded-full text-base-400',
+            'w-full px-4 py-4 rounded-full',
             'outline-none ring-2 ring-base-100',
             'focus:ring-primary-200',
-            isDarkMode ? 'bg-base-300 text-base-100 ring-base-200' : 'bg-base-100',
+            isDarkMode ? 'bg-base-300 text-base-100 ring-base-200' : 'bg-base-100 text-base-400',
             'pr-32' // Add padding to the right for the button
           ]"
           required
         />
         <button
           type="submit"
+          @click="handleSubmit"
           :disabled="isLoading"
           :class="[
             'absolute right-1 top-1 bottom-1',
@@ -30,19 +31,24 @@
         >
           {{ isLoading ? 'Joining...' : 'Join' }}
         </button>
-      </form>
-      <p v-if="message" :class="[
-        'mt-2 text-sm',
-        messageType === 'success' ? 'text-green-500' : 'text-red-500'
-      ]">
-        {{ message }}
-      </p>
+      </div>
+      <div class="h-8 relative">
+          <p v-if="message" :class="[
+            'absolute w-full mt-2 text-sm text-center',
+            messageType === 'success' ? 'text-primary-400' : 'text-red'
+          ]">
+          {{ message }}
+        </p>
+    </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { collection, addDoc, doc } from 'firebase/firestore'
+import { useCollection } from 'vuefire'
+import { db } from '@/firebaseConfig.js'
 
 const { isDarkMode } = defineProps(['isDarkMode'])
 
@@ -51,23 +57,57 @@ const isLoading = ref(false)
 const message = ref('')
 const messageType = ref('')
 
+watch(message, () => {
+  setTimeout(() => {
+    message.value = ''
+    messageType.value = ''
+  }, 5000)
+})
+
+const validateEmail = (email) => {
+  // Regular expression for email validation
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
+
 const handleSubmit = async () => {
-  if (!email.value) return
+  if (!email.value) {
+    message.value = 'Please enter your email address.'
+    messageType.value = 'error'
+    isLoading.value = false
+    return
+  }
+
+  if(!validateEmail(email.value)){
+    message.value = 'Invalid email address'
+    messageType.value = 'error'
+    isLoading.value = false
+    return
+  }
+
+  const waitlistCollection = collection(db, 'waitlist')
+  const waitlist = useCollection(waitlistCollection)
 
   isLoading.value = true
   message.value = ''
   messageType.value = ''
 
+  const data = {
+    email: email.value,
+    created_at: new Date().toISOString()
+  }
+
   try {
-    // Simulating an API call
-    await new Promise(resolve => setTimeout(resolve, 1500))
+    const response = await addDoc(waitlistCollection, data)
     
-    // Simulated success response
+    if (!response) {
+      throw new Error('An error occurred')
+    }    
+    
     message.value = 'You have been added to the waiting list!'
     messageType.value = 'success'
     email.value = ''
   } catch (error) {
-    // Simulated error handling
     message.value = 'An error occurred. Please try again.'
     messageType.value = 'error'
   } finally {
--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueFire } from 'vuefire';
+import { firebaseApp } from './firebaseConfig';
 
-const app = createApp(App)
+const app = createApp(App);
 
+app.use(VueFire, { firebaseApp });
 app.use(router)
 
 app.mount('#app')
