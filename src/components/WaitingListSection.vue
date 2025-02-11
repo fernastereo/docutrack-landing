34c8cff97a4617042a9369088329ac9e5f90a0f1
Fernando Cueto <fernandoecueto@gmail.com>
2025-02-12 00:24:40 +0100

    feat: update title in index.html and enhance layout in WaitingListSection component

--- src/components/WaitingListSection.vue
+++ src/components/WaitingListSection.vue
@@ -3,7 +3,7 @@
     'py-12 px-4 transition-colors duration-300',
     isDarkMode ? 'bg-base-300' : 'bg-base-200'
   ]">
-    <div class="flex flex-col">
+    <div class="container mx-auto w-3/4 border-b-2 border-base-100 pb-8 flex flex-col">
       <h2 class="text-2xl font-semibold text-center">Ready to see through the legal fog?</h2>
       <p class="text-center my-6">Sign up now to join our waiting list and be the first to experience legal clarity like never before!</p>
       <WaitingList :isDarkMode="isDarkMode" />
