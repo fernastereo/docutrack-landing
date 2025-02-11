b6bab25bb614e3069cf6ee42b988a6bb6dfc4d68
Fernando Cueto <fernandoecueto@gmail.com>
2025-02-11 23:53:00 +0100

    feat: add NotFoundView component and configure router for 404 handling; update Vite base path

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import WaitlistView from '@/views/WaitlistView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/waitlist',
       name: 'waitlist',
       component: WaitlistView,
+    },
+    {
+      path: '/:catchall(.*)*',
+      name: 'not-found',
+      component: NotFoundView,
     },
   ],
 });
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { RouterLink } from 'vue-router';
+
+</script>
+<template>
+  <main class="grid min-h-full place-items-center bg-white px-6 py-32 ">
+    <div class="text-center">
+      <h2 class="text-5xl font-semibold text-green-600">404</h2>
+      <h1 class="mt-4 text-7xl font-semibold text-gray-900">Page not found</h1>
+      <p class="mt-6 text-lg font-medium text-gray-500">Sorry, we couldn’t find the page you’re looking for.</p>
+      <div class="mt-10">
+        <RouterLink to="/" class="rounded-md bg-green-600 px-3 py-3 font-semibold text-white hover:bg-green-500">Go back home</RouterLink>
+      </div>
+    </div>
+  </main>
+</template>
