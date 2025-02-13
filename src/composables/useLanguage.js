65dadd46cf5be988a1978f0bda963c942aa2e399
Fernando Cueto <fernandoecueto@gmail.com>
2025-02-14 00:13:52 +0100

    feat: track language selection event for user engagement

--- src/composables/useLanguage.js
+++ src/composables/useLanguage.js
@@ -1,12 +1,19 @@
 import { ref } from 'vue';
+import { useGtag } from 'vue-gtag-next';
 
+const { event } = useGtag();
 const language = ref('es');
 
 export const useLanguage = () => {
-  
   const setLanguage = (lang) => {
     language.value = lang;
-  }
 
-  return { language, setLanguage }
-}
+    event('language-selected', {
+      event_category: 'engagement',
+      event_label: 'language',
+      value: lang,
+    });
+  };
+
+  return { language, setLanguage };
+};
